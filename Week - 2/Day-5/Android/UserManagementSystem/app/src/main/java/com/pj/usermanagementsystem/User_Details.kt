ce635fdb329a52ad53904ff9658bc097297fe07d
Pratyush0803 <jenapratyush29@gmil.com>
2025-02-21 18:50:38 +0530

    Day5 Added

--- Week - 2/Day-5/Android/UserManagementSystem/app/src/main/java/com/pj/usermanagementsystem/User_Details.kt
+++ Week - 2/Day-5/Android/UserManagementSystem/app/src/main/java/com/pj/usermanagementsystem/User_Details.kt
@@ -11,10 +11,6 @@ class User_Details : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_user_details)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
     }
 }
