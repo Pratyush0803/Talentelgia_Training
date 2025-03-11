855c9c6bf4f5f018840950c6e46e8045eddc1c88
Pratyush0803 <jenapratyush29@gmil.com>
2025-03-11 09:36:34 +0530

    Day1 added

---
+++ Week - 5/Day-1/Dart/Final_and_Const.dart
@@ -0,0 +1,28 @@
+//FINAL:
+
+// void main(){
+//   final name = "Pratyush";
+//   print(name);
+
+
+//   final String name2 = "Pratyush Kumar Jena";
+//   print(name2);
+
+//   //If we reassign value to final it will throw ERROR "Error: Can't assign to the final variable 'name'."
+
+// //   name = "Bubu";
+// }
+
+// // CONST:
+void main(){
+  const name = "Pratyush";
+  print(name);
+
+
+  const String name2 = "Pratyush Kumar Jena";
+  print(name2);
+
+  //If we reassign value to final
+
+//   name = "Bubu";   //it will throw ERROR "Error: Can't assign to the const variable 'name'."
+}
