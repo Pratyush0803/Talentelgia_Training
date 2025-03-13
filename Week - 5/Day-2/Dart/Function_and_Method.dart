8364c78aea20db1231ebecd34d549091bf8f0269
Pratyush0803 <jenapratyush29@gmil.com>
2025-03-13 09:19:11 +0530

    Day2 added

---
+++ Week - 5/Day-2/Dart/Function_and_Method.dart
@@ -0,0 +1,12 @@
+void details(){
+  print("Name: Pratyush Kumar Jena");
+  print("Age: 21");
+}
+int add(int a, int b){
+  return a+b;
+}
+void main(){
+  details();
+  var result = add(10, 100);
+  print(result);
+}
